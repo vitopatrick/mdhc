7ed68eaf1b62e7c613a0d4823ab2f7eb89d09b44
warlock <fecypher@gmail.com>
2024-07-28 23:48:58 +0100

    chat

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,25 +12,25 @@ import Work from "../components/Work/Work";
 import Plan from "../components/Plan/Plan";
 
 const Home = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
-    const token = sessionStorage.getItem("token");
+    // Function to load Tidio Chat
+    const loadTidioChat = () => {
+      const tidioScript = document.createElement("script");
+      tidioScript.src = "//code.jivosite.com/widget/2Vdujj5j9s";
+      tidioScript.async = true;
+      document.body.appendChild(tidioScript);
+    };
 
-    if (token) {
-      navigate("/dashboard");
-    }
-    var Tawk_API = Tawk_API || {},
-      Tawk_LoadStart = new Date();
-    (function () {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = "https://embed.tawk.to/6576407470c9f2407f7e1d7e/1hhauni44";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode.insertBefore(s1, s0);
-    })();
+    // Load Tidio Chat when component mounts
+    loadTidioChat();
+
+    // Clean up function to remove Tidio Chat when component unmounts
+    return () => {
+      const tidioElement = document.getElementById("tidio-chat");
+      if (tidioElement) {
+        tidioElement.remove();
+      }
+    };
   }, []);
 
   return (
@@ -49,4 +49,3 @@ const Home = () => {
 };
 
 export default Home;
-
