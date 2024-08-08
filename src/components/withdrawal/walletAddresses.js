825b0e50c7432b4f4cebee819951a429dd5d96f4
warlock <fecypher@gmail.com>
2024-08-08 18:09:38 +0100

    changed logo

--- src/components/withdrawal/walletAddresses.js
+++ src/components/withdrawal/walletAddresses.js
@@ -1,6 +1,6 @@
 import BtcLogo from "../../img/btc.png";
 import ethLogo from "../../img/eth.png";
-import ltcLogo from "../../img/ltc.png";
+import usdtLogo from "../../img/usdt.png";
 
 export const wallets = [
   {
@@ -14,8 +14,8 @@ export const wallets = [
     icon: ethLogo,
   },
   {
-    coin: "Litecoin",
-    addressName: "LTC Address",
-    icon: ltcLogo,
+    coin: "USDT",
+    addressName: "USDT Address",
+    icon: usdtLogo,
   },
 ];
